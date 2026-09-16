0f68ff4e088385041992b5673401a1be1db364b3
Serhiy <120639693+Sergiy2k1@users.noreply.github.com>
2026-09-16 21:14:25 +0300

    fix: add HTTP status codes import to auth flow tests

--- tests/TaskManager.Api.IntegrationTests/AuthenticationFlowTests.cs
+++ tests/TaskManager.Api.IntegrationTests/AuthenticationFlowTests.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TaskManager.Api.Contracts;
 using TaskManager.Api.Contracts.Auth;
