d687f66a5c32c18ce81c8529c4f06f2463621734
Serhiy <120639693+Sergiy2k1@users.noreply.github.com>
2026-09-16 21:23:30 +0300

    fix: add ProblemDetails namespace import

--- TaskManager.Api/DependencyInjection.cs
+++ TaskManager.Api/DependencyInjection.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using TaskManager.Api.Authentication;
